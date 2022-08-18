5ea21389afc6d6c6537e155acd269ddf77c8a10b
Transit-Lumber <Tristan-Bulmer@live.uc.nmit.nz>
2022-08-18 22:26:30 +1200

    Added simple js and css

--- src/app.js
+++ src/app.js
@@ -1,13 +1,3 @@
-deleteBtn.addEventListener('click', e => {
-    //let id = e.target.parentElement.getAttribute('data-id');
-    //db.collection('todo').doc(id).delete();
-})
+const addBtn = document.getElementById('add');
 
-addBtn.addEventListener('click', e => {
-    
-})
-
-updateBtn.addEventListener('click', e => {
-    //let id = e.target.parentElement.getAttribute('data-id');
-    //db.collection('todo').doc(id)
-})
+addBtn.addEventListener('click', ()=> addNewNote());
